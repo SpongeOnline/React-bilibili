09865749d65cc3ff704e4f4cbe9a570d84dad829
Chuang Wang <chuangwang@microsoft.com>
2022-11-20 02:26:30 +0800

    Create react router demo project

---
+++ react-with-router/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+import "./App.css";
+import {CktButtonCallBack} from "../components/button/CktButton"
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+        
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
