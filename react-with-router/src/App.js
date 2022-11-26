618c18e33c45cf3c349ede1590b0bab5c9868a0c
Chuang Wang <chuangwang@microsoft.com>
2022-11-26 23:24:19 +0800

    Almost do nothing

--- react-with-router/src/App.js
+++ react-with-router/src/App.js
@@ -2,12 +2,9 @@ import {hot} from "react-hot-loader";
 import React, { Component} from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./modules/layout";
-// import TestButton from "./modules/testButton";
+import TestButton from "./modules/testButton";
 import { fluentButton, provideFluentDesignSystem } from "@fluentui/web-components"
 provideFluentDesignSystem().register(fluentButton());
-
-const TestButton = asyncComponent(() => import("./modules/testButton"));
-
 
 class App extends Component{
   render(){
@@ -22,33 +19,5 @@ class App extends Component{
     );
   }
 }
-
-function asyncComponent(importComponent) {
-  class AsyncComponent extends Component {
-    constructor(props) {
-      super(props);
- 
-      this.state = {
-        component: null
-      };
-    }
- 
-    async componentDidMount() {
-      const { default: component } = await importComponent();
- 
-      this.setState({
-        component: component
-      });
-    }
- 
-    render() {
-      const C = this.state.component;
- 
-      return C ? <C {...this.props} /> : null;
-    }
-  }
- 
-  return AsyncComponent;
- }
 
 export default hot(module)(App);
