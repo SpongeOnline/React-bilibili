7b454d0d23e9044be52237f55624d897ec104fa0
Chuang Wang <chuangwang@microsoft.com>
2022-11-27 16:34:38 +0800

    Added a button for chrome extension

--- tab-manager/src/modules/testButton.js
+++ tab-manager/src/modules/testButton.js
@@ -1,14 +1,29 @@
 import React from "react"
 
-const testButton = () => {
+class TestButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "123",
+    };
+    this.clickButton = ()=> {
+      alert("Ckt is a good man.")
+    }
+  }
+
+  render() {
     return (
       <div>
-        {/* <button type="button">
-            I'm button
-        </button> */}
-        <fluent-button>test button</fluent-button>
+          <button onClick={() => { 
+              this.props.onClick()
+          }}>
+              {this.state.value}
+          </button>
+          <button onClick={this.clickButton}></button>
       </div>
+      
     );
-  };
-  
-  export default testButton;
+  }
+}
+
+export default TestButton;
